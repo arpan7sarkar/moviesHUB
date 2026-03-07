0c0e90c21d041ba90a4791936d3ac0fade64ec6d
arpan7sarkar <arpansakar@gmail.com>
2026-03-07 23:24:05 +0530

    initalized the server side , installing all the dependecies

---
+++ server/src/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect to Database
+connectDB();
+
+// Middleware
+app.use(helmet());
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  credentials: true
+}));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Base Route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to MoviesHUB API' });
+});
+
+// Error Handling Middleware (Placeholder)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
